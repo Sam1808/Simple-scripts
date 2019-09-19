9918a30bf28d491e3e0d30e313891425ba12a8c9
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2019-09-19 19:52:18 +0300

    Add features

--- Instagram.space.bot/main.py
+++ Instagram.space.bot/main.py
@@ -1,14 +1,10 @@
+import argparse
 import os
 import requests
 from dotenv import load_dotenv
 from instabot import Bot
 from os import listdir
 
-path = input('Please enter folder name (default is Images): ')
-spacex_url = input('Please enter SpaceX_URL (default is latest launch): ')
-spacex_choice = input('Would you like to run downloading SpaceX`s photos? (Yes/No): ')
-image_id = input('Please enter Hubble`s image ID (default is 1000):')
-hubble_choice = input('Would you like to run downloading Hubble`s photos? (Yes/No):' )
 
 def get_extension(link):
     separation_link = os.path.splitext(link)
@@ -28,51 +24,57 @@ def fetch_spacex_last_launch(spacex_url):
     for element in enumerate(catalog):
         file_number = str(element[0])
         file_link = element[1]
-        filename = 'spacex_' + file_number + '.jpg'
-        download_image(file_link, filename)
+        file_name = 'spacex_' + file_number + '.jpg'
+        download_image(file_link, file_name)
 
 def fetch_habble_image(image_id):
+    habble_url = 'http://hubblesite.org/api/v3/image/' + str(image_id)
     response = requests.get(habble_url)
     catalog = response.json()['image_files']
     for element in reversed(catalog):
         last_link = 'https:' + element.get('file_url')
         if get_extension(last_link) == '.jpg': #download only JPG
-            filename = 'image_id_' + str(image_id) + str(get_extension(last_link))
-            download_image(last_link, filename)
+            file_name = 'image_id_' + str(image_id) + str(get_extension(last_link))
+            download_image(last_link, file_name)
             break
+
+def get_instagram_credentials():
+    load_dotenv()
+    insta_login = os.getenv("INSTA_LOGIN")
+    insta_pass = os.getenv("INSTA_PASS")
+    if not insta_login:
+        insta_login = input('Please enter your Instagram Login: ')
+        insta_pass = input('Please enter your Instagram Password: ')
+    credentials = {'login':insta_login, 'password':insta_pass}
+    return (credentials)
 
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description='Instagram space bot')
+    parser.add_argument('-p', '--path', help='''Folder name for pictures (default is 'Images')''')
+    parser.add_argument('-s', '--space_url', help='URL with http(s) prefix to download SpaceX images')
+    parser.add_argument('-ha', '--habble_id', help='Hubble`s image ID')
+    args = parser.parse_args()
+
+    path = args.path
     if not path:
         path = 'Images'
     os.makedirs(path, exist_ok = True)
     abspath = os.path.abspath(path)
 
-    if not spacex_url:
-        spacex_url = 'https://api.spacexdata.com/v3/launches/latest'
+    if args.space_url:
+        fetch_spacex_last_launch(args.space_url)
 
-    if spacex_choice == 'Yes':
-        fetch_spacex_last_launch(spacex_url)
-
-    if not image_id:
-        image_id = 1000
-    habble_url = 'http://hubblesite.org/api/v3/image/' + str(image_id)
-
-    if hubble_choice == 'Yes':
-        fetch_habble_image(image_id)
+    if args.habble_id:
+        fetch_habble_image(args.habble_id)
 
     pictures = listdir(path)
 
-    if len(pictures) != 0:
-        load_dotenv()
-        INSTA_LOGIN = os.getenv("INSTA_LOGIN")
-        INSTA_PASS = os.getenv("INSTA_PASS")
-        if not INSTA_LOGIN:
-            INSTA_LOGIN = input('Please enter your Instagram Login: ')
-            INSTA_PASS =  input('Please enter your Instagram Password: ')
+    if len(pictures):
         bot = Bot()
-        bot.login(username=INSTA_LOGIN, password=INSTA_PASS)
+        credentials = get_instagram_credentials()
+        bot.login(username=credentials.get('login'), password=credentials.get('password'))
         for pic in pictures:
             photo_path = os.path.join(abspath,pic)
             print('Image will be upload: ', photo_path)
