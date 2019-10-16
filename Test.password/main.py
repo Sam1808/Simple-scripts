80bb8efc7b884aa693f29387f995175689604b36
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2019-10-16 19:40:41 +0300

    Steps 16-19

--- Test.password/main.py
+++ Test.password/main.py
@@ -1,50 +1,31 @@
 import urwid
 
 def has_digit(password):
-    for character in password:
-        if character.isdigit():
-            return True
-    return False
+    return any(character.isdigit() for character in password)
 
 def has_letters(password):
-    for character in password:
-        if character.isalpha():
-            return True
-    return False
+    return any(character.isalpha() for character in password)
 
 def is_very_long(password):
     if len(password) > 12:
         return True
-    else:
-        return False
+    return False
 
 def has_upper_letters(password):
-    for character in password:
-        if character.isupper():
-            return True
-    return False
+    return any(character.isupper() for character in password)
 
 def has_lower_letters(password):
-    for character in password:
-        if not character.isupper():
-            return True
-    return False
+    return any(not character.isupper() for character in password)
 
 def has_symbols(password):
-    for character in password:
-        if not character.isdigit() and not character.isalpha():
-            return True
-    return False
+    return any(not character.isalnum() for character in password)
 
 def has_not_only_symbols(password):
-    for character in password:
-        if not has_symbols(character):
-            return True
-    return False
+    return any(not has_symbols(character) for character in password)
 
-def test_password(edit, password):
+def verification_password(edit, password):
     score = 0
-    set_of_tests = [
+    set_of_verifications = [
         has_lower_letters,
         has_upper_letters,
         has_letters,
@@ -53,8 +34,8 @@ def test_password(edit, password):
         has_not_only_symbols,
         has_symbols,
     ]
-    for test in set_of_tests:
-       if test(password):
+    for verification in set_of_verifications:
+       if verification(password):
            score += 2
     native_password.set_text("Your password: %s" % password)
     total_score.set_text("Password rating: %s" % score)
@@ -65,6 +46,6 @@ if __name__ == '__main__':
     native_password = urwid.Text('')
     total_score = urwid.Text('')
     menu = urwid.Pile([password, native_password, total_score])
-    urwid.connect_signal(password, 'change', test_password)
+    urwid.connect_signal(password, 'change', verification_password)
     menu = urwid.Filler(menu, valign='top')
     urwid.MainLoop(menu).run()
