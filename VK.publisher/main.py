de7a7d032bdd0741b31c6d322d20103db2af3066
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2019-10-16 17:08:34 +0300

    Steps 17-21

--- VK.publisher/main.py
+++ VK.publisher/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import random
 import requests
@@ -6,42 +7,59 @@ from dotenv import load_dotenv
 
 
 def download_image(url, filename):
-    print('Run downloading...', filename)
+    logging.info(u'Downloading file. ')
     response = requests.get(url)
     with open(filename, 'wb') as file:
         file.write(response.content)
 
+def get_last_comics_number():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    last_number = response.json()['num']
+    return last_number
+
+def download_comics(comics_number):
+    url = f'https://xkcd.com/{comics_number}/info.0.json'
+    response = requests.get(url)
+    set_of_data = response.json()
+    comics_url = set_of_data.get('img')
+    comics_description = set_of_data.get('alt')
+    comics_filename = f'comic-{comics_number}.png'
+    download_image(comics_url, comics_filename)
+    return comics_filename, comics_description
+
+def get_vk_response(method, payload):
+    url = f'https://api.vk.com/method/{method}'
+    response = requests.get(url, params=payload)
+    try:
+        response = response.json()['response']
+        return response
+    except:
+        response = response.json()['error']
+        error_message = response.get('error_msg')
+        logging.error(f'VK error: {error_message}')
+        os.remove(comics_filename)
+        exit()
+
+def post_request(url, params, files):
+    response = requests.post(url, params=params, files=files)
+    return response.json()
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='VK photo auto publisher')
-    parser.add_argument('-lc', '--last_comics', help='Please add current last number comics (default:2212)')
     parser.add_argument('-t', '--token', help='Add your own VK Token')
     parser.add_argument('-id', '--group_id', help='Add your own VK Group ID')
+
     args = parser.parse_args()
 
     comics_number_from = 1
-
-    comics_number_till = args.last_comics
-
-    if not comics_number_till:
-        comics_number_till = 2212 # October 2019
-
-    comics_number_till = int(comics_number_till)
-
+    comics_number_till = get_last_comics_number()
     comics_number = random.randint(comics_number_from,comics_number_till)
 
-    url = f'https://xkcd.com/{comics_number}/info.0.json'
-    response = requests.get(url)
-    comics_url = response.json()['img']
-    comics_description = response.json()['alt']
-    comics_filename = f'comic-{comics_number}.png'
-    download_image(comics_url, comics_filename)
+    comics_filename, comics_description = download_comics(comics_number)
 
-    print(f'''
-        Ok, {comics_filename} will be published.
-        Getting link to upload comics and upload it...
-        ''')
+    print(f'Ok, {comics_filename} will be published.')
 
     load_dotenv()
     vk_token = args.token
@@ -50,41 +68,31 @@ if __name__ == '__main__':
         vk_token = os.getenv("VK_TOKEN")
 
     method = 'photos.getWallUploadServer'
-    url = f'https://api.vk.com/method/{method}'
-
     payload = {'access_token': vk_token, 'v': 5.101, 'no_agreement': 1}
 
-    response = requests.get(url, params=payload)
-    response = response.json()['response']
+    response = get_vk_response(method, payload)
 
     with open(comics_filename, 'rb') as file:
         url = response.get('upload_url')
         files = {'photo': file}
-        response = requests.post(url, files=files)
-        response.raise_for_status()
+        response = post_request(url,None,files)
 
-    server = response.json()['server']
-    photo = response.json()['photo']
-    hash = response.json()['hash']
-
-    payload.update({'server':server, 'photo':photo, 'hash': hash})
-
-    print('Saving photo for VK Wall...')
+    server = response.get('server')
+    photo = response.get('photo')
+    received_hash = response.get('hash')
 
     method = 'photos.saveWallPhoto'
+    payload.update({'server':server, 'photo':photo, 'hash': received_hash})
     url = f'https://api.vk.com/method/{method}'
 
-    response = requests.post(url, params=payload)
-    response.raise_for_status()
+    response = post_request(url, payload, None)
 
-    response =response.json()['response']
-    set_of_parameters = response[0]
+    set_of_parameters = response['response'][0]
     media_id = set_of_parameters.get('id')
     owner_id = set_of_parameters.get('owner_id')
     attachments = f'photo{owner_id}_{media_id}'
 
     group_id = args.group_id
-
     if not group_id:
         group_id = os.getenv("GROUP_ID")
 
@@ -93,6 +101,7 @@ if __name__ == '__main__':
     del payload['photo']
     del payload['hash']
 
+    method = 'wall.post'
     payload.update({
         'owner_id': group_id,
         'message': comics_description,
@@ -100,15 +109,7 @@ if __name__ == '__main__':
         'from_group': 1,
     })
 
-    print('Publishing photo at VK Wall...')
+    response = get_vk_response(method,payload)
 
-    method = 'wall.post'
-    url = f'https://api.vk.com/method/{method}'
-
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-
-    print(f'Removing {comics_filename} local file...')
     os.remove(comics_filename)
-
     print('Please check your new post at the Wall. Good buy.')
