47b7a0a813d5231324e9a6760aa4accb9f0a0b7b
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2019-10-22 01:40:55 +0300

    steps 17-20
    Please let me know more about step 20

--- VK.publisher/main.py
+++ VK.publisher/main.py
@@ -1,49 +1,169 @@
-import urwid
+import argparse
+import logging
+import os
+import random
+import requests
+from dotenv import load_dotenv
 
-def has_digit(password):
-    return any(character.isdigit() for character in password)
 
-def has_letters(password):
-    return any(character.isalpha() for character in password)
+def download_image(url, filename):
+    logging.info(f'Downloading file {filename}')
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
-def is_very_long(password):
-    return len(password) > 12
+def get_last_comics_number():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    last_number = response.json()['num']
+    return last_number
 
-def has_upper_letters(password):
-    return any(character.isupper() for character in password)
+def get_comics_properties(comics_number):
+    url = f'https://xkcd.com/{comics_number}/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    comics_properties = response.json()
+    return comics_properties
 
-def has_lower_letters(password):
-    return any(not character.isupper() for character in password)
+def get_vk_response(method, payload):
+    url = f'https://api.vk.com/method/{method}'
+    response = requests.get(url, params=payload)
+    response = response.json()
+    if 'error' in response:
+        inform_error(response)
+        return None
+    response = response.get('response')
+    return response
 
-def has_symbols(password):
-    return any(not character.isalnum() for character in password)
+def post_request(url, params, files):
+    response = requests.post(url, params=params, files=files)
+    response = response.json()
+    if 'error' in response:
+        inform_error(response)
+        return None
+    return response
 
-def has_not_only_symbols(password):
-    return any(not has_symbols(character) for character in password)
+def get_vk_token(vk_token):
+    load_dotenv()
+    if not vk_token:
+        vk_token = os.getenv("VK_TOKEN")
+    return vk_token
 
-def verification_password(edit, password):
-    score = 0
-    set_of_verifications = [
-        has_lower_letters,
-        has_upper_letters,
-        has_letters,
-        has_digit,
-        is_very_long,
-        has_not_only_symbols,
-        has_symbols,
-    ]
-    for verification in set_of_verifications:
-       if verification(password):
-           score += 2
-    native_password.set_text("Your password: %s" % password)
-    total_score.set_text("Password rating: %s" % score)
+def get_vk_group_id(group_id):
+    load_dotenv()
+    if not group_id:
+        group_id = os.getenv("GROUP_ID")
+    return group_id
+
+def inform_error(response):
+    response = response.get('error')
+    error_message = response.get('error_msg')
+    logging.error(f'VK error: {error_message}')
+
+
+
 
 if __name__ == '__main__':
 
-    password = urwid.Edit('Enter password: ', mask='*')
-    native_password = urwid.Text('')
-    total_score = urwid.Text('')
-    menu = urwid.Pile([password, native_password, total_score])
-    urwid.connect_signal(password, 'change', verification_password)
-    menu = urwid.Filler(menu, valign='top')
-    urwid.MainLoop(menu).run()
+    parser = argparse.ArgumentParser(description='VK photo auto publisher')
+    parser.add_argument('-t', '--token', help='Add your own VK Token')
+    parser.add_argument('-id', '--group_id', help='Add your own VK Group ID')
+
+    args = parser.parse_args()
+
+    comics_number_from = 1
+
+    try:
+        comics_number_till = get_last_comics_number()
+    except requests.exceptions.ConnectionError:
+        logging.error(f'Connection Error: Please check your Internet connection and try later.')
+        exit()
+
+    comics_number = random.randint(comics_number_from,comics_number_till)
+
+    try:
+        comics_properties = get_comics_properties(comics_number)
+    except requests.exceptions.ConnectionError:
+        logging.error(f'Connection Error: Please check your Internet connection and try later.')
+        exit()
+
+    comics_url = comics_properties.get('img')
+    comics_description = comics_properties.get('alt')
+    comics_filename = f'comics-{comics_number}.png'
+
+    try:
+        download_image(comics_url, comics_filename)
+    except requests.exceptions.ConnectionError:
+        logging.error(f'Connection Error: Please check your Internet connection and try later.')
+        exit()
+
+    print(f'Ok, {comics_filename} will be published.')
+
+    vk_token = args.token
+    group_id = args.group_id
+
+    if not vk_token:
+        logging.info('VK Token was not provided. Trying get credentials from .env file...')
+        vk_token = get_vk_token(vk_token)
+
+    if not group_id:
+        logging.info('VK Group ID was not provided. Trying get credentials from .env file...')
+        group_id = get_vk_group_id(group_id)
+
+    method = 'photos.getWallUploadServer'
+    payload = {'access_token': vk_token, 'v': 5.101, 'no_agreement': 1}
+
+    response = get_vk_response(method, payload)
+    if not response:
+        os.remove(comics_filename)
+        exit()
+
+    with open(comics_filename, 'rb') as file:
+        url = response.get('upload_url')
+        files = {'photo': file}
+        response = post_request(url,None,files)
+        if not response:
+            os.remove(comics_filename)
+            exit()
+
+    server = response.get('server')
+    photo = response.get('photo')
+    received_hash = response.get('hash')
+
+    method = 'photos.saveWallPhoto'
+    payload.update({'server':server, 'photo':photo, 'hash': received_hash})
+
+    url = f'https://api.vk.com/method/{method}'
+
+    response = post_request(url, payload, None)
+    if not response:
+        os.remove(comics_filename)
+        exit()
+
+    set_of_parameters = response['response'][0]
+    media_id = set_of_parameters.get('id')
+    owner_id = set_of_parameters.get('owner_id')
+    attachments = f'photo{owner_id}_{media_id}'
+
+    del payload['no_agreement']
+    del payload['server']
+    del payload['photo']
+    del payload['hash']
+
+    method = 'wall.post'
+    payload.update({
+        'owner_id': group_id,
+        'message': comics_description,
+        'attachments': attachments,
+        'from_group': 1,
+    })
+
+    response = get_vk_response(method,payload)
+    if not response:
+        os.remove(comics_filename)
+        exit()
+
+    os.remove(comics_filename)
+    print('Please check your new post at the Wall. Good buy.')
